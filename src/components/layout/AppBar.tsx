a4dad5e76c3e1f81ed1092c4651d5aca454c0e23
devofficer <nikolaytoplev@gmail.com>
2022-03-23 21:38:00 +0000

    Implement AppBar UI

--- src/components/layout/AppBar.tsx
+++ src/components/layout/AppBar.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import WalletConnector from '../widgets/WalletConnector';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 const CustomAppBar: React.FC = () => {
   return (
-    <>
-      <WalletConnector />
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Rock Paper Scissors
+          </Typography>
+          <WalletConnector />
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
