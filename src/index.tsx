c1c46abf9083841ab43f446c152775806d45d65a
devofficer <nikolaytoplev@gmail.com>
2022-03-23 17:38:27 +0000

    Setup mui theme provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import App from './App';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
