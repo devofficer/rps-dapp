bfe1f60c9efa0c5445bfc2a047c3ecfeb2cee740
devofficer <nikolaytoplev@gmail.com>
2022-03-23 22:39:42 +0000

    Implement WalletConnector

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import AppBar from './components/layout/AppBar';
 import AppContainer from './components/layout/AppContainer';
+import { UseWalletProvider } from 'use-wallet';
 
 function App() {
   return (
-    <>
+    <UseWalletProvider>
       <AppBar />
       <AppContainer />
-    </>  
+    </UseWalletProvider>
   );
 }
 
--- src/components/widgets/WalletConnector.tsx
+++ src/components/widgets/WalletConnector.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import Button from '@mui/material/Button';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import { useWallet } from 'use-wallet';
 
 const WalletConnector: React.FC = () => {
+  const wallet = useWallet();
+
+  const handleConnect = async (event: React.MouseEvent<HTMLElement>) => {
+    await wallet.connect('injected');
+  };
 
   return (
-    <></>
+    <>
+      <Button
+        variant="contained"
+        color="secondary"
+        startIcon={<AccountBalanceWalletIcon />}
+        onClick={handleConnect}
+      >
+        Connect Wallet
+      </Button>
+    </>
   );
 };
 
