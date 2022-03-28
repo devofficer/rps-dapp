45d15a77de16d7f4d73b4499403fba91523dfba0
devofficer <nikolaytoplev@gmail.com>
2022-03-28 12:56:28 +0000

    Add CreatedGame route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GameBoard from 'components/pages/GameBoard';
+import CreatedGame from 'components/pages/CreatedGame';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<GameBoard />} />
+        <Route path="/created-game/:addr" element={<CreatedGame />} />
       </Routes>
     </BrowserRouter>
   );
